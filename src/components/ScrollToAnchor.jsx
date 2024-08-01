779187abbdb94285df86e9325a8440c22f6a774d
Landon Grinderslev <125698291+lgrinders@users.noreply.github.com>
2024-08-01 16:20:07 -0500

    deleted unneeded files

--- src/components/ScrollToAnchor.jsx
+++ src/components/ScrollToAnchor.jsx
@@ -6,7 +6,6 @@ export function ScrollToAnchor() {
   const lastHash = useRef("");
 
   // listen to location change using useEffect with location as dependency
-  // https://jasonwatmore.com/react-router-v6-listen-to-location-route-change-without-history-listen
   useEffect(() => {
     if (location.hash) {
       lastHash.current = location.hash.slice(1); // safe hash for further use after navigation
